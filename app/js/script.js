b9ff5efc3726715937b55c3b672fdc267ba6ab31
Denis <corvus007@yandex.ru>
2017-07-10 15:22:02 +0300

    Добавялет скрипты блока js-accordion для faq

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   svg4everybody();
+
 
 
   $('.reviews__slider [data-fancybox]').fancybox({
@@ -37,33 +38,64 @@ document.addEventListener('DOMContentLoaded', function() {
     }]
   });
 
-});
+
+
+  $('.gallery [data-fancybox]').fancybox({
+    loop: false,
+    afterMove: function (instance, slide) {
+      $('.gallery-slider').slick('slickGoTo', instance.currIndex - 1);
+    }
+  });
+
+  $('.gallery-slider').slick({
+    accessibility: false,
+    dots: true,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1290,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 567,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    }]
+  });
 
 
 
-$('.gallery-slider [data-fancybox]').fancybox({
-  loop: false,
-  afterShow: function (instance, slide) {
-    $('.gallery-slider').slick('slickGoTo', instance.currIndex - 1);
+  /*=================================
+  =            Accordion            =
+  =================================*/
+
+  var $accordion = $('.js-accordion');
+
+  if ($accordion.length) {
+    $accordion.find('dd').hide();
+    $accordion.on('click', 'dt', function (event) {
+      event.preventDefault();
+
+      $accordion
+        .find('dt')
+        .not($(this))
+        .removeClass('is-opened')
+        .next('dd')
+        .slideUp();
+
+      if (!$(this).hasClass('is-opened')) {
+        $(this).addClass('is-opened');
+        $(this).next('dd').stop().slideDown();
+      } else {
+        $(this).removeClass('is-opened');
+        $(this).next('dd').stop().slideUp();
+      }
+    });
   }
-});
 
-$('.gallery-slider').slick({
-  accessibility: false,
-  dots: true,
-  infinite: false,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  responsive: [{
-    breakpoint: 1290,
-    settings: {
-      slidesToShow: 3
-    }
-  }, {
-    breakpoint: 567,
-    settings: {
-      slidesToShow: 1,
-      arrows: false
-    }
-  }]
+  /*=====  End of Accordion  ======*/
 });
