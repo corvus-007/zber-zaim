9adea29ba76f90bc9b5cc7557028a698ecb7e147
Denis <corvus007@yandex.ru>
2017-07-10 12:50:35 +0300

    Добавляет скрипты для элемента reviews__slider

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
   svg4everybody();
-  console.log('script.js');
+
+
+  $('.reviews__slider [data-fancybox]').fancybox({
+    transitionEffect: 'slide',
+    loop: true,
+    infobar: true,
+    afterShow: function (instance, slide) {
+      $('.reviews__slider').slick('slickGoTo', instance.currIndex - 1);
+    }
+  });
+
+  $('.reviews__slider').slick({
+    accessibility: false,
+    dots: true,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1300,
+      settings: {
+        slidesToShow: 5
+      }
+    }, {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 567,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    }]
+  });
+
 });
