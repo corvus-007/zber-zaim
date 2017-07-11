6408471a0dfb937dc42d09b910a6724ca16d8932
Denis <corvus007@yandex.ru>
2017-07-11 19:38:56 +0300

    Создание скрипта для блока calculator

--- app/js/script.js
+++ app/js/script.js
@@ -69,6 +69,90 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+  $('.js-tabs').tabslet();
+
+
+
+  var monthSlider = document.querySelector('#month-slider');
+  var startRange = 3;
+
+  var formatter = new Intl.NumberFormat('ru', {
+    style: 'currency',
+    currency: 'RUB',
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+  });
+
+  noUiSlider.create(monthSlider, {
+    start: startRange,
+    step: 1,
+    connect: [true, false],
+    tooltips: wNumb({
+      postfix: ' мес.',
+      decimals: 0
+    }),
+    range: {
+      'min': 1,
+      'max': 24
+    }
+  });
+
+  var handle = monthSlider.querySelector('.noUi-handle');
+
+  handle.setAttribute('tabindex', 0);
+
+  handle.addEventListener('click', function () {
+    this.focus();
+  });
+
+  handle.addEventListener('keydown', function (e) {
+    var value = Number(monthSlider.noUiSlider.get());
+
+    switch (e.which) {
+      case 37:
+        monthSlider.noUiSlider.set(value - 1);
+        break;
+      case 39:
+        monthSlider.noUiSlider.set(value + 1);
+        break;
+    }
+  });
+
+
+  function calculateTarif() {
+    return (calculateCoef * amountValue * period + 200);
+  }
+
+  var calculatorTarif = 'every-month';
+  var tarifs = {
+    'every-month': 1.2,
+    'save': 2.8
+  };
+  var calculateCoef = tarifs['every-month'];
+  var amountValue = $('.calculator-form__input')[0].value;
+  var period = startRange;
+  var calculateOutput = document.querySelector('.calculator-form__output');
+
+  calculateTarif();
+
+  $('.calculator-form').on('change', '.calculator-form__radio-input', function (event) {
+    calculateCoef = tarifs[this.value];
+    calculateOutput.value = formatter.format(calculateTarif());
+  });
+
+  $('.calculator-form').on('input', '.calculator-form__input', function (event) {
+    console.log('input');
+    amountValue = this.value;
+    calculateOutput.value = formatter.format(calculateTarif());
+  });
+
+  monthSlider.noUiSlider.on('update', function (values, handle) {
+    period = values[handle];
+    calculateOutput.value = formatter.format(calculateTarif());
+  });
+
+
+
   /*=================================
   =            Accordion            =
   =================================*/
@@ -100,7 +184,7 @@ document.addEventListener('DOMContentLoaded', function () {
   /*=====  End of Accordion  ======*/
 
 
-  $(window).on('load', function() {
+  $(window).on('load', function () {
     $('.photos-users__list').jMosaic({
       min_row_height: 200,
       margin: 1
